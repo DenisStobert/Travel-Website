e1f0d0de88ce4de828316d5b6828bc9633cf7839
Denis Stobert <den.stobert1@gmail.com>
2024-04-22 19:32:49 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,76 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__form", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+// trending container
+ScrollReveal().reveal(".trending__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// destination container
+ScrollReveal().reveal(".destination__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+// seller container
+ScrollReveal().reveal(".seller__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// guide container
+ScrollReveal().reveal(".guide__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+//  client container
+ScrollReveal().reveal(".client__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal('.step', {
+  ...scrollRevealOption,
+  interval: 300
+});
+function initAutocomplete() {
+  var fromInput = document.getElementById("fromAirport");
+  var toInput = document.getElementById("toAirport");
+
+  var options = {
+    types: ["(airports)"],
+    componentRestrictions: { country: "us" },
+  };
+
+  var autocompleteFrom = new google.maps.places.Autocomplete(
+    fromInput,
+    options
+  );
+  var autocompleteTo = new google.maps.places.Autocomplete(toInput, options);
+}
+
+// Load the script asynchronously
+function loadGooglePlacesScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src =
+    "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initAutocomplete";
+  document.body.appendChild(script);
+}
+
+window.onload = loadGooglePlacesScript;
